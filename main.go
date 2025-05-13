b61d344fa6c063f100200ecc71322d3e41f0514a
gainax2k1 <gainax2k1@gmail.com>
2025-05-12 17:30:17 -0700

    testing aded

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
-    fmt.Println("Hello, World!")
-    
+	fmt.Println("Hello, World!")
+
 }
 
 func cleanInput(text string) []string {
+	//creates "lowered", trimming  trailing whitespace and making lowercase
+	lowered := strings.ToLower(strings.TrimSpace(text))
 
-    return []string{} // placeholder
+	words := strings.Fields(lowered)
+
+	return words // placeholder
 }
--- repl_test.go
+++ repl_test.go
@@ -1,41 +1,58 @@
+package main
 
-
+import (
+	"testing"
+)
 
 func TestCleanInput(t *testing.T) {
-    // ...
-    // start by creating a slice of structs:
+	// ...
+	// start by creating a slice of structs:
 
-    cases := []struct{
-        input string
-        expected []string
-    }{
-            {
-                input: "  hello world  ",
-                expected: []string{"hello", "world"},
-            },
-            // add more cases here
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "this is a test",
+			expected: []string{"this", "is", "a", "test"},
+		},
+		{
+			input:    " ThIs Is A cAmEl TeSt ",
+			expected: []string{"this", "is", "a", "camel", "test"},
+		},
+		{
+			input:    " tOO     MuCh    whiTESPace     ",
+			expected: []string{"too", "much", "whitespace"},
+		},
 
+		// add more cases here
 
-    }
+	}
 
-    // then loop over cases and run tests
+	// then loop over cases and run tests
 
-    for _, c := range cases {
-        actual := cleanInput(c.input)
-        // check the actual length vs expected
-        // if not a match, use t.Errorf to print msg and fail test
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// check the actual length vs expected
+		if len(actual) != len(c.expected) {
+			t.Errorf("cleanInput(%q) return %d words, expected %d", c.input, len(actual), len(c.expected))
+			continue
+		}
+		// if not a match, use t.Errorf to print msg and fail test
 
-        for i := range actual {
-            word := actual[i]
-            expectedWord := c.expected[i]
-            // check each word in slice
-            // if no match, uses t.Errorf.....
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// check each word in slice
+			// if no match, uses t.Errorf.....
+			if word != expectedWord {
+				t.Errorf("cleanInput(%q)[%d] = %q, expected %q", c.input, i, word, expectedWord)
+			}
+		}
+	}
 
-            }
-        }
-    
-
-        
-
-    
 }
